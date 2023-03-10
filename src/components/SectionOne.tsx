59657998ee63c6ec1507e480c45563ebe447a8f8
Doug Kellermeyer <doug.kellermeyer@gmail.com>
2023-03-10 14:59:21 -0500

    made header and footer responsive

--- src/components/SectionOne.tsx
+++ src/components/SectionOne.tsx
@@ -2,7 +2,19 @@ import * as React from "react"
 
 const SectionOne = () => {
     return (
-        <div style={{border: "1px solid red", height: "600px"}}>section one</div>
+        <div className="section-one">
+            <div className="section-one-wrapper">
+                <div className="section-one-left">
+
+                </div>
+                <div className="section-one-right">
+
+                </div>
+            </div>
+            <div className="section-one-bottom">
+                
+            </div>
+        </div>
     )
 }
 
