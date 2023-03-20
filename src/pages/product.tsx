11639505325c87a62b100b5b9aa5701142e0a6eb
Doug Kellermeyer <doug.kellermeyer@gmail.com>
2023-03-20 17:05:08 -0400

    next section of flow done TODO make icon images not-blurry svgs are actually pngs :/

--- src/pages/product.tsx
+++ src/pages/product.tsx
@@ -2,6 +2,8 @@ import * as React from "react"
 import Layout from "../components/layout"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
+// @ts-ignore
+import dataFlowGIF from "../images/product-flow-animation.gif"
 
 
 const ProductPage = () => {
@@ -28,92 +30,94 @@ const ProductPage = () => {
                         </div>
                     </div>
                 </div>
-                <div className="product-flow-section-two">
-                    <div className="product-flow-section-two-left">
-                        <StaticImage
-                            placeholder="none"
-                            alt="estuary logo"
-                            src="../images/estuary-vertical-logo.svg"
-                            layout="fixed"
-                            className="product-flow-section-one-image"
-                        />
-                        <div>left image</div>
-                    </div>
-                    <div className="product-flow-section-two-right">
-                        <div>
-                            <p className="product-flow-section-two-header-small">Data Movement</p>
-                            <h2 className="product-flow-section-two-topic-header">Data capture</h2>
-                            <p className="product-flow-section-two-topic-subtext">Capture your data from sources like databases and SaaS applications.</p>
-                            <div>
-                                <div>
-                                    <StaticImage
-                                        placeholder="none"
-                                        alt="data flow image"
-                                        src="../images/product-flow-section-two-data-capture-image1.svg"
-                                        layout="fixed"
-                                        className="product-flow-section-one-image"
-                                    />
-                                    <p className="product-flow-section-two-topic-child">Real-time CDC from databases. Built by the Estuary team to scale 
-                                        and ensure exactly once semantics.</p>
-                                </div>
-                                <div>
-                                    <StaticImage
-                                        placeholder="none"
-                                        alt="salesforce logo"
-                                        src="../images/product-flow-section-two-data-capture-salesforce-logo.svg"
-                                        layout="fixed"
-                                        className="product-flow-section-one-image"
-                                    />
-                                    <p className="product-flow-section-two-topic-child">
-                                        Real-time SaaS integrations built by the Estuary team for apps that support streaming.
-                                    </p>
-                                </div>
-                                <div>
-                                    <StaticImage
-                                        placeholder="none"
-                                        alt="estuary logo"
-                                        src="../images/product-flow-section-two-data-capture-image2.svg"
-                                        layout="fixed"
-                                        className="product-flow-section-one-image"
-                                    />
-                                    <p className="product-flow-section-two-topic-child">
-                                        Long-tail SaaS. Integrate any Airbyte connector to access 200+ batch-based endpoints.
-                                    </p>
-                                </div>
+                <div className="product-flow-section-two-background-image-wrapper">
 
+                    <div className="product-flow-section-two">
+                        <div className="product-flow-section-two-left">
+                            <StaticImage
+                                placeholder="none"
+                                alt="estuary logo"
+                                src="../images/estuary-vertical-logo.svg"
+                                layout="fixed"
+                                className="product-flow-section-one-image"
+                            />
+                            <div className="product-flow-section-two-gif-wrapper">
+                                <img className="product-flow-section-two-gif" src={dataFlowGIF} alt="database data flow gif animation" />
                             </div>
                         </div>
-                        <div>
-                            <p className="product-flow-section-two-header-small">Tranformations</p>
-                            <h2 className="product-flow-section-two-topic-header">Powerful transformations</h2>
-                            <p className="product-flow-section-two-topic-subtext">True streaming SQL and Javascript transformations.</p>
+                        <div className="product-flow-section-two-right">
                             <div>
+                                <p className="product-flow-section-two-header-small">Data Movement</p>
+                                <h2 className="product-flow-section-two-topic-header">Data capture</h2>
+                                <p className="product-flow-section-two-topic-subtext">Capture your data from sources like databases and SaaS applications.</p>
+                                <div className="product-flow-topic-section-wrapper">
+                                    <div className="product-flow-topic-wrapper">
+                                        <StaticImage
+                                            placeholder="none"
+                                            alt="data flow image"
+                                            src="../images/product-flow-section-two-data-capture-image1.svg"
+                                            layout="fixed"
+                                            className="product-flow-section-two-topic-image"
+                                        />
+                                        <p className="product-flow-section-two-topic-child"><span className="product-flow-bold">Real-time CDC </span>from databases. Built by the Estuary team to scale
+                                            and ensure exactly once semantics.</p>
+                                    </div>
+                                    <div className="product-flow-topic-wrapper">
+                                        <StaticImage
+                                            placeholder="none"
+                                            alt="salesforce logo"
+                                            src="../images/product-flow-section-two-data-capture-salesforce-logo.svg"
+                                            layout="fixed"
+                                            className="product-flow-section-two-topic-image"
+                                        />
+                                        <p className="product-flow-section-two-topic-child">
+                                            <span className="product-flow-bold">Real-time SaaS integrations</span>  built by the Estuary team for apps that support streaming.
+                                        </p>
+                                    </div>
+                                    <div className="product-flow-topic-wrapper">
+                                        <StaticImage
+                                            placeholder="none"
+                                            alt="data cloud gear image"
+                                            src="../images/product-flow-section-two-data-capture-image2.svg"
+                                            layout="fixed"
+                                            className="product-flow-section-two-topic-image"
+                                        />
+                                        <p className="product-flow-section-two-topic-child">
+                                            <span className="product-flow-bold">Long-tail SaaS</span>. Integrate any Airbyte connector to access 200+ batch-based endpoints.
+                                        </p>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <div className="product-flow-section-two-transformations-wrapper">
+                                <p className="product-flow-section-two-header-small">Transformations</p>
+                                <h2 className="product-flow-section-two-topic-header">Powerful transformations</h2>
+                                <div className="product-flow-topic-wrapper">
+                                    <StaticImage
+                                        placeholder="none"
+                                        alt="circle to square flow image"
+                                        src="../images/product-flow-section-two-topic-image3.svg"
+                                        layout="fixed"
+                                        className="product-flow-section-two-topic-image"
+                                    />
+                                    <p className="product-flow-section-two-topic-child">True streaming SQL and Javascript transformations.</p>
+                                </div>
+                            </div>
+                            <div>
+                                <h2 className="product-flow-section-two-topic-header">Materializations</h2>
+                                <div className="product-flow-topic-wrapper">
                                     <StaticImage
                                         placeholder="none"
                                         alt="estuary logo"
-                                        src="../images/estuary-vertical-logo.svg"
+                                        src="../images/product-flow-section-two-clock-icon.svg"
                                         layout="fixed"
-                                        className="product-flow-section-one-image"
+                                        className="product-flow-section-two-topic-image"
                                     />
-                                    <p>
-                                        Long-tail SaaS. Integrate any Airbyte connector to access 200+ batch-based endpoints.
+                                    <p className="product-flow-section-two-topic-child">
+                                        Maintain low-latency views across systems of your choice. Built by the Estuary team, all materialization destinations are natively real-time.
                                     </p>
                                 </div>
-                        </div>
-                        <div>
-                            <h2 className="product-flow-section-two-topic-header">Materializations</h2>
-                            <div>
-                                    <StaticImage
-                                        placeholder="none"
-                                        alt="estuary logo"
-                                        src="../images/estuary-vertical-logo.svg"
-                                        layout="fixed"
-                                        className="product-flow-section-one-image"
-                                    />
-                                    <p>
-                                    Maintain low-latency views across systems of your choice. Built by the Estuary team, all materialization destinations are natively real-time.
-                                    </p>
-                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
