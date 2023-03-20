a1a7b583a83e69019ec77952c66845d1992062e1
Doug Kellermeyer <doug.kellermeyer@gmail.com>
2023-03-20 11:14:11 -0400

    added background images for flow product

--- src/pages/product.tsx
+++ src/pages/product.tsx
@@ -8,22 +8,24 @@ const ProductPage = () => {
     return (
         <Layout headerTheme="light">
             <div className="product-flow">
-                <div className="product-flow-section-one">
-                    <div className="product-flow-section-one-left">
-                        <h1 className="product-flow-section-one-h1">Streaming ELT made simple</h1>
-                        <p className="product-flow-section-one-subtext">Use it free with hands-on support. Scale without bounds.</p>
-                        <Link className="product-flow-section-one-try-it-button" to="#">
-                            Try it free
-                        </Link>
-                    </div>
-                    <div className="product-flow-section-one-right">
-                        <StaticImage
-                            placeholder="none"
-                            alt="estuary logo"
-                            src="../images/estuary-vertical-logo.svg"
-                            layout="fixed"
-                            className="product-flow-section-one-image"
-                        />
+                <div className="product-flow-section-one-background-image-wrapper">
+                    <div className="product-flow-section-one">
+                        <div className="product-flow-section-one-left">
+                            <h1 className="product-flow-section-one-h1">Streaming ELT made simple</h1>
+                            <p className="product-flow-section-one-subtext">Use it free with hands-on support. Scale without bounds.</p>
+                            <Link className="product-flow-section-one-try-it-button" to="#">
+                                Try it free
+                            </Link>
+                        </div>
+                        <div className="product-flow-section-one-right">
+                            <StaticImage
+                                placeholder="none"
+                                alt="estuary logo"
+                                src="../images/estuary-vertical-logo.svg"
+                                layout="fixed"
+                                className="product-flow-section-one-image"
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
