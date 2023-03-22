7934f6331bf61a9c05eb4f53dddb1906ea1e9182
Joseph Shearer <joseph@josephshearer.net>
2023-03-22 17:55:09 +0100

    feature: Pull testimonials and logos from CMS

--- src/components/SectionFive.tsx
+++ src/components/SectionFive.tsx
@@ -1,9 +1,34 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import { useStaticQuery, graphql } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import { GatsbyImage, StaticImage } from "gatsby-plugin-image"
 
 const SectionFive = () => {
+    const testimonials = useStaticQuery(graphql`
+        {
+            allStrapiTestimonial(sort: {SortOrder: DESC}, filter: {Enabled: {eq: true}}) {
+                nodes {
+                    name: Name
+                    body: Text
+                    enabled: Enabled
+                    logo: Logo {
+                        localFile {
+                            svg {
+                                content # SVG content optimized with SVGO
+                            }
+                            name
+                            internal {
+                                mediaType
+                            }
+                            childImageSharp {
+                                gatsbyImageData(layout: FIXED, width: 30)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    `)
     return (
         <div className="section-five">
             <div className="section-five-header-wrapper">
@@ -11,123 +36,24 @@ const SectionFive = () => {
                 <h2 className="section-five-header">What our customers say</h2>
             </div>
             <div className="section-five-tile-wrapper">
-                <div className="section-five-tile">
-                    <p className="section-five-tile-text">
-                        “We're a big fan of Estuary's real-time, no code model.
-                        It's magic that we're getting real time data without
-                        much effort and we don't have to spend time thinking
-                        about broken pipelines. We've also experienced fantastic
-                        support by Estuary, sometimes they go as far as to make
-                        a change for us if it's easier for them to do so."
-                    </p>
-                    <div className="section-five-logo-wrapper">
-                        <StaticImage
-                            placeholder="none"
-                            alt="Flashpack"
-                            objectFit="contain"
-                            src="../images/flashpack-logo.png"
-                            className="section-five-header-vector"
-                            layout="fixed"
-                            imgStyle={{ filter: "brightness(0) invert(1)" }}
-                        />
-                        <p className="section-five-tile-logo-text">Flashpack</p>
-                    </div>
-                </div>
-                <div className="section-five-tile">
-                    <p className="section-five-tile-text">
-                        “Estuary Flow allows us to integrate low-latency CDC and
-                        connect to SaaS apps across our entire reporting stack
-                        and it’s the only solution that we’ve found that lets us
-                        do both.”
-                    </p>
-                    <div className="section-five-logo-wrapper">
-                        <StaticImage
-                            placeholder="none"
-                            alt="estuary logo top"
-                            objectFit="contain"
-                            src="../images/deepsync-logo.png"
-                            className="section-five-header-vector"
-                            layout="fixed"
-                        />
-                        <p className="section-five-tile-logo-text">Deep Sync</p>
-                    </div>
-                </div>
-                <div className="section-five-tile">
-                    <p className="section-five-tile-text">
-                        “Estuary is the only system we’ve found that can
-                        seamlessly replicate large scale Firestore data for
-                        analytics.  After months of research and trying
-                        everything, we can confidently say that Estuary is the
-                        only company that can help us get easy, accurate
-                        analytics on our data within Snowflake."
-                    </p>
-                    <div className="section-five-logo-wrapper">
-                        <StaticImage
-                            placeholder="none"
-                            alt="estuary logo top"
-                            objectFit="contain"
-                            src="../images/coalesce-logo.png"
-                            className="section-five-header-vector"
-                            layout="fixed"
-                        />
-                        <p className="section-five-tile-logo-text">Coalesce</p>
-                    </div>
-                </div>
-                <div className="section-five-tile">
-                    <p className="section-five-tile-text">
-                        “This tool is 1000x times better than LogStash or
-                        Elastic Enterprise Data Ingestion Tool, which has many
-                        issues.”
-                    </p>
-                    <div className="section-five-logo-wrapper">
-                        <StaticImage
-                            placeholder="none"
-                            alt="estuary logo top"
-                            objectFit="contain"
-                            src="../images/pompato-logo.png"
-                            className="section-five-header-vector"
-                            layout="fixed"
-                        />
-                        <p className="section-five-tile-logo-text">Pompato</p>
-                    </div>
-                </div>
-                <div className="section-five-tile">
-                    <p className="section-five-tile-text">
-                        “We needed a platform to help us optimize marketing
-                        campaigns with low-latency.  Estuary provided an
-                        unparalleled solution to do that at terabyte scale."
-                    </p>
-                    <div className="section-five-logo-wrapper">
-                        <StaticImage
-                            placeholder="none"
-                            alt="estuary logo top"
-                            objectFit="contain"
-                            src="../images/fenestra-logo.png"
-                            className="section-five-header-vector"
-                            layout="fixed"
-                        />
-                        <p className="section-five-tile-logo-text">Fenestra</p>
-                    </div>
-                </div>
-                <div className="section-five-tile">
-                    <p className="section-five-tile-text">
-                        “It's very rewarding to find a cloud product actually
-                        works when the configuration is correct.”
-                    </p>
-                    <div className="section-five-logo-wrapper">
-                        <StaticImage
-                            placeholder="none"
-                            alt="estuary logo top"
-                            objectFit="contain"
-                            src="../images/a-avatar.png"
-                            className="section-five-header-vector"
-                            layout="fixed"
-                        />
+                {testimonials.allStrapiTestimonial.nodes.map(node => (
+                    <div className="section-five-tile">
+                        <p className="section-five-tile-text">{node.body}</p>
+                        <div className="section-five-logo-wrapper">
+                            {node.logo.localFile.internal.mediaType === "image/svg+xml" ? <div style={{width:24}} dangerouslySetInnerHTML={{__html:node.logo.localFile.svg.content}}/> :
+                            <GatsbyImage
+                                alt={`${node.name} logo`}
+                                image={
+                                    node.logo.localFile.childImageSharp
+                                        .gatsbyImageData
+                                }
+                            />}
                         <p className="section-five-tile-logo-text">
-                            Anna Bansaghi, Engineer
+                            {node.name}
                         </p>
+                        </div>
                     </div>
-                </div>
+                ))}
             </div>
         </div>
     )
