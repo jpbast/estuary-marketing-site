379d9213927bc1d67a2926815c7b46e64fa1f799
ykycha-kolot <55743745+yuriyspz@users.noreply.github.com>
2023-06-07 23:11:12 -0400

    no figma UI on the page

--- src/pages/demo.tsx
+++ src/pages/demo.tsx
@@ -7,7 +7,7 @@ const LpPodcats = () => {
         <Layout headerTheme="light">
             <div className="frame-wrap">
                 <iframe
-                    src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FxsxbnLUnqIhrz00xQssxck%2FEstuary%3Fpage-id%3D233%253A2%26type%3Ddesign%26node-id%3D238-403%26viewport%3D980%252C298%252C0.21%26scaling%3Dmin-zoom%26starting-point-node-id%3D238%253A403"
+                    src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FxsxbnLUnqIhrz00xQssxck%2FEstuary%3Ftype%3Ddesign%26node-id%3D238-403%26scaling%3Dscale-down-width%26page-id%3D233%253A2%26starting-point-node-id%3D238%253A403&hide-ui=1"
                     allowFullScreen
                 ></iframe>
             </div>
