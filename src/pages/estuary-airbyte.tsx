acb3a4b3b2961a4d74fdd097d7eb08c160f4275a
ykycha-kolot <ykycha.kolot@gmail.com>
2023-06-17 10:59:50 -0400

    estuary-airbyte + small updates

--- src/pages/estuary-airbyte.tsx
+++ src/pages/estuary-airbyte.tsx
@@ -2,8 +2,8 @@ import * as React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import SignUp from "../components/signup"
-import HeroImageDesktop from "../svgs/estuary-vs-fivetran.svg"
-import HeroImageMobile from "../svgs/estuary-vs-fivetran-mobile.svg"
+import HeroImageDesktop from "../svgs/estuary-airbyte.svg"
+import HeroImageMobile from "../svgs/estuary-airbyte-mobile.svg"
 import FiveTran from "../svgs/fivetran-logo.svg"
 import Confluent from "../svgs/confluent-logo.svg"
 import Airbyte from "../svgs/airbyte-logo.svg"
@@ -15,7 +15,7 @@ const comparisonContent = [
         estuaryValue:
             "Self-serve streaming data platform for building real-time pipelines. Company behind <a href='https://github.com/gazette/core' target='_blank'>Gazette</a> and <a href='https://github.com/estuary/flow' target='_blank'>Estuary Flow OSS</a>",
         competitorValue:
-            "Early Modern Data Stack  vendor of batch ETL software for SaaS data pipelines",
+            "Large provider of <a href='https://github.com/airbytehq'>open-source</a> batch ELT data integration connectors + a fully managed platform",
         mattersValue: "n/a",
     },
     {
@@ -23,7 +23,7 @@ const comparisonContent = [
         estuaryValue:
             "Open-Source, or predictably priced pipelines at $1.50 / GB of change events with 10gb free",
         competitorValue:
-            "Based on volume of 'Monthly Active Rows' which can be highly vlatile",
+            "Based on <a href='https://airbyte.com/pricing'>credits</a>: <br/><br/>*$10 per gigabyte for DB<br/>*$15 per million rows for APIs",
         mattersValue:
             "Estuary is less than half the price for the same pipeline. Fivetran bills you exponentially for normalizing your data. (Read Fivetran pricing breakdown)",
     },
@@ -32,7 +32,7 @@ const comparisonContent = [
         estuaryValue:
             "<100ms. Only constraint is frequency of updates from the source, or what the destination can handle.",
         competitorValue:
-            "Latency depends on plan/price. Minimum possible latency is 5 minutes, which is <a href='https://www.fivetran.com/pricing'>2x the price</a> of the 1 hour latency.",
+            "Minimum possible latency is 5 minutes",
         mattersValue:
             "Faster data leads to 2x better marketing outcomes (Gartner) and enables real-time ML and insights.",
     },
@@ -41,63 +41,63 @@ const comparisonContent = [
         estuaryValue:
             "<a href='https://estuary.dev/integrations/'>100+ connectors</a>. Also HTTP file, webhook, and ability to spin up most new connectors within a week",
         competitorValue:
-            "<a href='https://www.fivetran.com/connectors'>340+</a> connectors. Also file-based ingestion and webhooks. Custom connector builder available. ",
+            "<a href='https://airbyte.com/connectors'>340+</a> connectors. 50+ of which are GA. Also file-based ingestion and webhooks. Custom connector builder available.",
         mattersValue:
-            "Fivetran has more connectors ready today, but new requests are added to a long queue. Even popular ones like S3 took <a href='https://support.fivetran.com/hc/en-us/community/posts/1500000583602-New-Destination-AWS-S3-destination'>over 2 years</a>.",
+            "Airbyte connectors are community buily. This enables a large library but also meaning quality can be inconsistent w/o modifications. Estuary will pull in Airbyte connetors that pass our QA.",
     },
     {
         featureName: "On-Prem",
         estuaryValue: "Coming Fall 2023",
-        competitorValue: "Yes",
+        competitorValue: "Yes. OSS.",
         mattersValue: "Diff tools for on-prem needed if using Estuary Flow",
     },
     {
         featureName: "Delivery",
         estuaryValue: "Exactly-Once",
-        competitorValue: "Exactly-Once",
+        competitorValue: "At-least Once",
         mattersValue:
-            "Duplicates can be created in the consumer without exactly once, creating excess cost",
-    },
-    {
-        featureName: "Schema Migrations",
-        estuaryValue: "Automated Schema Evolution",
-        competitorValue: "Automated Schema Evolution",
-        mattersValue:
-            "If source data rarely changes, no problem. If it changes often, there will be manual work to update each consumer schema",
+            "Duplicates can be created in the consumer, creating excess cost.Else, each consumer will need a de-dupe and may have inaccurate results",
     },
     {
         featureName: "ETL",
         estuaryValue: "Yes",
         competitorValue: "ELT Only.",
         mattersValue:
-            "Extraneous data loaded to warehouse driving more compute expense ",
+            "Extraneous data loaded to warehouse driving more compute expense",
+    },
+    {
+        featureName: "Schema Migrations",
+        estuaryValue: "Automated Schema Evolution",
+        competitorValue: "Monitors for schema evolution every 24 hours and alerts users on changes. Modifications not propagated downstream",
+        mattersValue:
+            "If source data rarely changes, no problem. If it changes often, there will be manual work to update each consumer schema",
     },
     {
         featureName: "Data Lake",
         estuaryValue:
             "Ingested data stored in a <a href='https://docs.estuary.dev/concepts/collections/'>real-time data lake</a> in customers cloud storage",
         competitorValue:
-            "Every new pipeline is made from scratch and requires creating a new ingest",
+            "Every new pipeline is made from scratch and begins with a new capture",
         mattersValue:
-            "By storing data in a real-time data lake, you can endlessly distribute off one ingest... saving you egress fees, MAR, and source system stress",
+            "By storing data in a real-time data lake, you can endlessly distribute off one ingest... saving you egress fees, credits, and source system stress",
     },
     {
         featureName: "Transforms",
         estuaryValue:
             "Streaming SQL transforms and joins on both real-time and history data. DBT as a destination",
-        competitorValue: "SQL transforms and DBT integration",
+        competitorValue: "Transforms take place in your warehouse with DBT",
         mattersValue:
-            "If data needs to be joined in flight, you'll need additional stream processing platform",
+            "If data needs to be joined in flight, you'll need an additional stream processing platform and your warehousing costs will go up",
     },
     {
         featureName: "Vector DBs & AI",
         estuaryValue: "Pinecone",
-        competitorValue: "None",
+        competitorValue: "Weaviate",
         mattersValue: "Teams quickly demanding support for vector DBs",
     },
 ]
 
-const EstuaryVsFivetran = () => {
+const EstuaryVsAirbyte = () => {
     const [isMobile, setMobile] = React.useState(false)
     const checkIfMobile = () =>
         typeof window !== "undefined" && window.innerWidth < 845
@@ -123,7 +123,7 @@ const EstuaryVsFivetran = () => {
                             <div className="hero-heading">
                                 Estuary Flow
                                 <br /> vs
-                                <br /> Fivetran
+                                <br /> Airbyte
                             </div>
                             <div className="hero-subheading">
                                 A COMPARISON OF ETL/ELT PLATFORMS
@@ -137,24 +137,12 @@ const EstuaryVsFivetran = () => {
                             )}
                         </div>
                     </div>
-                    <div className="hero-image-wrap">
-                        <StaticImage
-                            placeholder="none"
-                            alt="Pricing comparison"
-                            loading="lazy"
-                            src="../images/lp-comparison/pricing-comparison-estuary-fivetran.png"
-                            layout="constrained"
-                            width={1266}
-                            height={581}
-                            quality={100}
-                        />
-                    </div>
                 </section>
                 <section className="comparison-table">
                     <div className="table-heading">
                         <div className="heading-item">FEATURES</div>
                         <div className="heading-item">ESTUARY</div>
-                        <div className="heading-item">FIVETRAN</div>
+                        <div className="heading-item">AIRBYTE</div>
                         <div className="heading-item">Why it matters</div>
                     </div>
                     <div className="table-data">
@@ -277,4 +265,4 @@ export const Head = () => {
     )
 }
 
-export default EstuaryVsFivetran
+export default EstuaryVsAirbyte
