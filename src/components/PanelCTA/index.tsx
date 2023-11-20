e641cbbd212980bbd74df4356095a95f68cde2db
Flavio Riper <flavioriper_andrade@hotmail.com>
2023-11-20 12:17:26 -0300

    feat: add new slack and webinar svg

--- src/components/PanelCTA/index.tsx
+++ src/components/PanelCTA/index.tsx
@@ -1,7 +1,8 @@
 import React from "react"
 import { OutboundLink } from "gatsby-plugin-google-gtag"
 
-import SlackIcon from "../../svgs/slack-outline.svg"
+import SlackIcon from "../../svgs/slack-colors.svg"
+import WebinarIcon from "../../svgs/webinar-colors.svg"
 
 const PanelCTA = () => {
     return (
@@ -12,13 +13,13 @@ const PanelCTA = () => {
                     <OutboundLink href="https://try.estuary.dev/webinar_realtime_101/">Join Next Webinar</OutboundLink>
                 </div>
                 <div className="icon-content">
-                    <SlackIcon className="social-icon" />
+                    <WebinarIcon className="social-icon social-icon-2xl" />
                 </div>
             </div>
             <div className="sided-panel-cta">
                 <div className="text-content">
                     <p>Questions?</p>
-                    <OutboundLink href="https://try.estuary.dev/webinar_realtime_101/">Join our Slack</OutboundLink>
+                    <OutboundLink href="https://estuary-dev.slack.com/join/shared_invite/zt-86nal6yr-VPbv~YfZE9Q~6Zl~gmZdFQ#/shared-invite/email">Join our Slack</OutboundLink>
                 </div>
                 <div className="icon-content">
                     <SlackIcon className="social-icon" />
