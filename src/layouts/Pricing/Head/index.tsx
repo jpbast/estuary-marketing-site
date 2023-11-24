82673a481e2506872ef0fc5e848782d21aec577b
Flavio Riper <flavioriper_andrade@hotmail.com>
2023-11-24 16:22:22 -0300

    feat: add tag.clearbits to pricing

--- src/layouts/Pricing/Head/index.tsx
+++ src/layouts/Pricing/Head/index.tsx
@@ -9,7 +9,9 @@ export const Head = ({ data: { post } }) => {
             description={
                 "Reduce your data costs and latency with managed streaming CDC and ETL pipelines."
             }
-        />
+        >
+            <script src="https://tag.clearbitscripts.com/v1/pk_4817f0c2cea3e0595f4602d5c2a8e556/tags.js" referrerPolicy="strict-origin-when-cross-origin" />
+        </Seo>
     )
 }
 
