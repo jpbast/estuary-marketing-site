6826855fcc5eef6ca941058b909bc2f685b3836e
Flavio Riper <flavioriper_andrade@hotmail.com>
2023-12-07 15:04:24 -0300

    feat: update obverser

--- src/hooks/useOnScreen.ts
+++ src/hooks/useOnScreen.ts
@@ -3,11 +3,11 @@ import { useState, useMemo, useEffect, RefObject } from "react"
 export default function useOnScreen(ref: RefObject<HTMLElement>) {
     const [isIntersecting, setIntersecting] = useState(false)
   
-    const observer = useMemo(() => new IntersectionObserver(
-        ([entry]) => setIntersecting(entry.isIntersecting)
-    ), [ref])
-  
     useEffect(() => {
+        const observer = useMemo(() => new IntersectionObserver(
+            ([entry]) => setIntersecting(entry.isIntersecting)
+        ), [ref])
+
         observer.observe(ref.current)
         return () => observer.disconnect()
     }, [])
