847d3fb75a2c6be729fa97eff004d2aec5dd9c03
Joseph Shearer <joseph@josephshearer.net>
2024-02-21 12:01:42 -0500

    fix typo

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -6,7 +6,7 @@ import Footer from "./footer"
 import TakeATour from "./TakeATour"
 
 const Layout = ({headerTheme, fixedHeader = true, showTour = false, children}: {headerTheme: "light"|"dark", fixedHeader?: boolean, showTour?: boolean, children: React.ReactNode|React.ReactNode[]}) => {
- (
+  return (
     <div className="global-wrapper">
       <Header fixedHeader={fixedHeader} />
       <main className={clsx(fixedHeader && "global-main-fixed-header")}>{children}</main>
