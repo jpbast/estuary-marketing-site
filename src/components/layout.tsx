a585da2a05f9f1244e966794cb466cd23da08d50
Joseph Shearer <joseph@josephshearer.net>
2024-02-22 16:16:36 -0500

    try putting the tour back

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,15 +5,33 @@ import Header from "./header"
 import Footer from "./footer"
 import TakeATour from "./TakeATour"
 
-const Layout = ({headerTheme, fixedHeader = true, showTour = false, children}: {headerTheme: "light"|"dark", fixedHeader?: boolean, showTour?: boolean, children: React.ReactNode|React.ReactNode[]}) => {
-  return (
-    <div className="global-wrapper">
-      <Header fixedHeader={fixedHeader} />
-      <main className={clsx(fixedHeader && "global-main-fixed-header")}>{children}</main>
-      <Footer />
-    </div>
-  )
+const Layout = ({
+    headerTheme,
+    fixedHeader = true,
+    showTour = false,
+    children,
+}: {
+    headerTheme: "light" | "dark"
+    fixedHeader?: boolean
+    showTour?: boolean
+    children: React.ReactNode | React.ReactNode[]
+}) => {
+    const [closeTour, setCloseTour] = React.useState(
+        typeof window !== "undefined" &&
+            !!localStorage.getItem("@estuary/closeTour")
+    )
+    return (
+        <div className="global-wrapper">
+            {showTour && !closeTour && (
+                <TakeATour onClose={() => setCloseTour(true)} />
+            )}
+            <Header fixedHeader={fixedHeader} />
+            <main className={clsx(fixedHeader && "global-main-fixed-header")}>
+                {children}
+            </main>
+            <Footer />
+        </div>
+    )
 }
 
 export default Layout
-
