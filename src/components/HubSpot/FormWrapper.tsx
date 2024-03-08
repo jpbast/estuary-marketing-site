8be2347c501a6210da5a5ebc629e4706cbf6b37b
Travis Jenkins <travis@estuary.dev>
2024-03-08 15:37:59 -0500

    Adding a submit handled so an error is not thrown.

--- src/components/HubSpot/FormWrapper.tsx
+++ src/components/HubSpot/FormWrapper.tsx
@@ -8,6 +8,7 @@ function HubSpotFormWrapper() {
             loading={<LinearProgress />}
             portalId={"8635875"}
             formId={"698e6716-f38b-4bd5-9105-df9ba220e29b"}
+            onSubmit={() => {}} //This is blank on purpose. Just here so an error is not thrown
         />
     )
 }
