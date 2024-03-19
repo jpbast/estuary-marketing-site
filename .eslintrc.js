2d1e7096bafaca18b00541b8e7131c50928c69b2
Flavio Riper Andrade <40703179+flavioriper@users.noreply.github.com>
2024-03-19 16:07:08 -0300

    Feat: Change the old Webinar promote bar to the new one and update some eslint rules (#232)
    * feat: add react-hook plugin to eslint
    
    * ref: change take a tour structure and labels
    
    * ESLint formatting
    
    * Adding a space at the end
    
    ---------
    
    Co-authored-by: Travis Jenkins <travis@estuary.dev>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,14 @@
 /* eslint-env node */
 
 module.exports = {
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
+        'plugin:react-hooks/recommended',
+    ],
     parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
+    plugins: ['@typescript-eslint', 'react-hooks'],
     root: true,
     rules: {
         '@typescript-eslint/no-unused-vars': 'error',
@@ -28,6 +33,7 @@ module.exports = {
         'react/display-name': 'off',
         'react/prop-types': 'off',
         'react/react-in-jsx-scope': 'off',
+        'react-hooks/rules-of-hooks': 'error',
         'react-hooks/exhaustive-deps': 'error',
     },
 };
